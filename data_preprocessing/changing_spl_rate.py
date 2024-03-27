0a5ab6dff202140ad3f074192a560f14f0c09547
DianSim <diana.simonyan.1994@gmail.com>
2024-03-27 14:48:31 +0400

    Made some changes

--- data_preprocessing/changing_spl_rate.py
+++ data_preprocessing/changing_spl_rate.py
@@ -1,15 +1,34 @@
 import os
 import subprocess
 import tqdm
+import soundfile as sf
+
+
 
 def change_sample_rate(dir, sample_rate, out_dir_name):
+    replace_name = dir.split(os.sep)[-2]
     for root, dirs, files in os.walk(dir):
-        out_dir = os.sep.join(root.split(os.sep)[:-1]+[out_dir_name])
-        os.makedirs(out_dir, exist_ok=True)
+        save_dir = root.replace(replace_name, out_dir_name)
+        os.makedirs(save_dir, exist_ok=True)
         for file in tqdm.tqdm(files):
-            command = f"ffmpeg -i '{os.path.join(root, file)}' -ar 16000 '{os.path.join(out_dir, file)}'"
-            result = subprocess.run(['ffmpeg', '-i', f'{os.path.join(root, file)}', '-ar', '16000', f'{os.path.join(out_dir, file)}'], capture_output=True)
+            command = f"ffmpeg -i '{os.path.join(root, file)}' -ar {sample_rate} '{os.path.join(save_dir, file)}'"
+            result = subprocess.run(['ffmpeg', '-i', f'{os.path.join(root, file)}', '-ar', f'{sample_rate}', f'{os.path.join(save_dir, file)}'], capture_output=True)
 
 
-source = '/Users/dianasimonyan/Desktop/Thesis/Implementation/datasets/backround_noises_ESC50/audio'
-change_sample_rate(source, 16000, 'ESC_50_16khz')
+def sample_rate_list(dir):
+    """returns the set of sample rates of the audios in the given folder"""
+    sample_rates = set()
+    for root, dirs, files in os.walk(dir):
+        for file in files:
+            if file[-4:] == '.wav':
+                signal, sr = sf.read(os.path.join(root, file))
+                sample_rates.add(sr)
+    return sample_rates
+
+
+# source = '/data/saten/diana/SpeakingRateEstimation/data/ESC-50-master/audio'
+# change_sample_rate(source, 16000, 'ESC-50_16khz')
+
+
+# --------------------------------Testing--------------------------------
+print(sample_rate_list('/data/saten/diana/SpeakingRateEstimation/data/LibriSpeechChuncked_sil_removed/train-clean-100-fast-augmented/train-clean-100'))
